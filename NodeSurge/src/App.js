ab5d9d55e34e72f543307e633579bfbc1257128c
AKHIL SHETTY M <akhilshettym2003@gmail.com>
2025-03-12 22:06:23 +0530

    Styles

--- NodeSurge/src/App.js
+++ NodeSurge/src/App.js
@@ -1,6 +1,6 @@
 "use client"
 
-import "./App.css"
+import "./styles/App.css"
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
 import Navbar from "./components/Proreact/Navbar"
 import TextForm from "./components/Proreact/TextForm"
